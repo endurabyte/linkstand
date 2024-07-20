3fc69bb0bb4c1250f1ef27ba77322910f22fc2ee
Doug Slater <doug@slater.dev>
2024-07-20 15:58:57 -0400

    Deploy app to s3 with GH action

--- src/App/make.ps1
+++ src/App/make.ps1
@@ -1,14 +1,19 @@
 # Stop script on any error
 $ErrorActionPreference = "Stop"
 
-$js = "elm.js"
-$min = "elm.min.js"
+mkdir dist
+
+$js = "tmp/elm.js"
+$min = "dist/elm.min.js"
 
 # Compile Elm code with optimization
-elm make "./src/Main.elm" --optimize --output=$js $args
+elm make "src/Main.elm" --optimize --output=$js $args
+
+npm install -g uglifycss
+uglifycss "src/styles.css" --output "dist/styles.css"
 
 # Minify the JavaScript using UglifyJS
-# npm install -g uglify-js
+npm install -g uglify-js
 uglifyjs $js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output $min
 
 # Get file sizes
@@ -18,3 +23,5 @@ $minifiedSize = (Get-Item $min).Length
 # Output file sizes
 Write-Output "Compiled size: $compiledSize bytes ($js)"
 Write-Output "Minified size: $minifiedSize bytes ($min)"
+
+cp "src/index.html" "dist/"
