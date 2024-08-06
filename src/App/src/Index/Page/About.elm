715c698a35ad80872aec5d4aade30d8b317aa9ad
Doug Slater <doug@slater.dev>
2024-08-05 21:04:36 -0400

    Fix LSP warnings. LSP auto format

--- src/App/src/Index/Page/About.elm
+++ src/App/src/Index/Page/About.elm
@@ -1,53 +1,62 @@
 module Page.About exposing
-  ( Model
-  , init
-  , Msg
-  , update
-  , view
-  )
+    ( Model
+    , Msg
+    , init
+    , update
+    , view
+    )
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+
 -- MODEL
 
+
 type Status
-  = Ok
+    = Ok
+
 
 type alias Model =
-  { status : Status }
+    { status : Status }
+
 
 init : ( Model, Cmd msg )
-init  =
-  ( Model Ok, Cmd.none )
+init =
+    ( Model Ok, Cmd.none )
+
 
 
 -- UPDATE
 
+
 type Msg
-  = Loaded
+    = Loaded
+
 
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
-  case msg of
-    Loaded ->
-      ( model, Cmd.none )
+    case msg of
+        Loaded ->
+            ( model, Cmd.none )
+
 
 
 -- VIEW
 
+
 view : Model -> Browser.Document msg
 view model =
-  --Debug.log "Page.About.view"
-  { title = "LinkStand | About"
-  , body = 
-    [ div [ class "container" ]
-      [ h1 [] [ text "About" ]
-      , p [] [ text "LinkStand is a web service to share URLs and track their use." ]
-      , a [ href "https://github.com/endurabyte/linkstand/" ] [ text "Source Code" ]
-      , a [ class "d-block mt-4", href "/" ] [ text "Back" ]
-      ]
-    ]
-  }
+    --Debug.log "Page.About.view"
+    { title = "LinkStand | About"
+    , body =
+        [ div [ class "container" ]
+            [ h1 [] [ text "About" ]
+            , p [] [ text "LinkStand is a web service to share URLs and track their use." ]
+            , a [ href "https://github.com/endurabyte/linkstand/" ] [ text "Source Code" ]
+            , a [ class "d-block mt-4", href "/" ] [ text "Back" ]
+            ]
+        ]
+    }
